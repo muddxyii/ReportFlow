85d41416510c1e39838e9321b4ba1d790a04b352
muddxyii <muddxyii@gmail.com>
2024-11-19 13:44:18 -0700

    Created BaseBackflowViewModel

---
+++ ViewModels/BaseBackflowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using ABFReportEditor.Util;
+
+namespace ABFReportEditor.ViewModels;
+
+public abstract class BaseBackflowViewModel : INotifyPropertyChanged
+{
+    private byte[]? _pdfData;
+    
+    // Common PDF data handling
+    public byte[]? PdfData
+    {
+        get => _pdfData;
+        set
+        {
+            _pdfData = value;
+            OnPropertyChanged(nameof(PdfData));
+        }
+    }
+
+    // Common command for navigation
+    public ICommand NextCommand { get; }
+
+    protected BaseBackflowViewModel()
+    {
+        NextCommand = new Command(async () => await OnNext());
+    }
+
+    // Common PDF loading logic with template method pattern
+    public void LoadPdfData(byte[] pdfBytes)
+    {
+        PdfData = pdfBytes;
+        var formFields = PdfUtils.ExtractPdfFormData(pdfBytes);
+        LoadFormFields(formFields);
+    }
+
+    // Template method to be implemented by derived classes
+    protected abstract void LoadFormFields(Dictionary<string, string> formFields);
+    
+    // Template method for navigation logic
+    protected abstract Task OnNext();
+
+    // INotifyPropertyChanged Implementation
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
