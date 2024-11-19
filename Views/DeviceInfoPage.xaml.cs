703ef5abfdc7b088803740d9230c7fffd47dee57
muddxyii <muddxyii@gmail.com>
2024-11-19 10:25:55 -0700

    Implemented View Model

--- Views/DeviceInfoPage.xaml.cs
+++ Views/DeviceInfoPage.xaml.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ABFReportEditor.ViewModels;
 
 namespace ABFReportEditor.Views;
 
+[QueryProperty(nameof(ViewModel), "ViewModel")]
 public partial class DeviceInfoPage : ContentPage
 {
+    private DeviceInfoViewModel? _viewModel;
+
+    public DeviceInfoViewModel? ViewModel
+    {
+        get => _viewModel;
+        set
+        {
+            _viewModel = value;
+            BindingContext = _viewModel;
+        }
+    }
+    
     public DeviceInfoPage()
     {
         InitializeComponent();
-    }
-
-    private void OnNextClicked(object? sender, EventArgs e)
-    {
-        throw new NotImplementedException();
     }
 }
