c13de1f5c0287a5cba08cbe514f56e0b5c47a503
muddxyii <muddxyii@gmail.com>
2024-11-30 11:57:37 -0700

    Changed BaseViewModel to BaseTestViewModel

--- ViewModels/TestViewModels/RpTestViewModel.cs
+++ ViewModels/TestViewModels/RpTestViewModel.cs
@@ -2,137 +2,13 @@ using ABFReportEditor.ViewModels.FinalViewModels;
 
 namespace ABFReportEditor.ViewModels.TestViewModels;
 
-public class RpTestViewModel : BaseBackflowViewModel
+public class RpTestViewModel : BaseTestViewModel
 {
-    #region Dropdown Items
-
-    public List<string> ShutoffValveOptions { get; } =
-    [
-        "BOTH OK", "BOTH CLOSED", "BOTH VALVES",
-        "#1 VALVE", "#2 VALVE"
-    ];
-
-    #endregion
-
-    #region Private properties
-
-    private string? _linePressure;
-    private string? _shutoffValve;
-    private string? _sovComment;
-    private string? _checkValve1;
-    private string? _checkValve2;
-    private string? _pressureReliefOpening;
-    private bool _checkValve1Leaked;
-    private bool _checkValve2Leaked;
-    private bool _reliefValveDidNotOpen;
-
-    #endregion
-
-    #region Public Properties
-
-    public string? LinePressure
+    protected override bool ValidateFields()
     {
-        get => _linePressure;
-        set
-        {
-            _linePressure = value;
-            OnPropertyChanged(nameof(LinePressure));
-        }
-    }
-    
-    public string? ShutoffValve
-    {
-        get => _shutoffValve;
-        set
-        {
-            _shutoffValve = value;
-            OnPropertyChanged(nameof(ShutoffValve));
-        }
-    }
-    
-    public string? SOVComment
-    {
-        get => _sovComment;
-        set
-        {
-            _sovComment = value;
-            OnPropertyChanged(nameof(SOVComment));
-        }
-    }
-
-    public string? CheckValve1
-    {
-        get => _checkValve1;
-        set
-        {
-            _checkValve1 = value;
-            OnPropertyChanged(nameof(LinePressure));
-        }
-    }
-
-    public string? CheckValve2
-    {
-        get => _checkValve2;
-        set
-        {
-            _checkValve2 = value;
-            OnPropertyChanged(nameof(_checkValve2));
-        }
-    }
-
-    public string? PressureReliefOpening
-    {
-        get => _pressureReliefOpening;
-        set
-        {
-            _pressureReliefOpening = value;
-            OnPropertyChanged(nameof(_pressureReliefOpening));
-        }
-    }
-
-    public bool CheckValve1Leaked
-    {
-        get => _checkValve1Leaked;
-        set
-        {
-            _checkValve1Leaked = value;
-            OnPropertyChanged(nameof(CheckValve1Leaked));
-        }
-    }
-
-    public bool CheckValve2Leaked
-    {
-        get => _checkValve2Leaked;
-        set
-        {
-            _checkValve2Leaked = value;
-            OnPropertyChanged(nameof(CheckValve2Leaked));
-        }
-    }
-
-    public bool ReliefValveDidNotOpen
-    {
-        get => _reliefValveDidNotOpen;
-        set
-        {
-            _reliefValveDidNotOpen = value;
-            OnPropertyChanged(nameof(ReliefValveDidNotOpen));
-        }
-    }
-
-    #endregion
-
-    #region Abstract function implementation
-
-    protected override void LoadFormFields(Dictionary<string, string> formFields)
-    {
-        // Init check valve leaked buttons as true
-        CheckValve1Leaked = true;
-        CheckValve2Leaked = true;
-    }
-
-    protected override async Task OnNext()
-    {
+        // Do base validation
+        if (!base.ValidateFields()) return false;
+        
         // List of required fields with their error message
         var requiredFields = new Dictionary<string, string>
         {
@@ -148,58 +24,14 @@ public class RpTestViewModel : BaseBackflowViewModel
             var propertyValue = GetType().GetProperty(field.Key)?.GetValue(this) as string;
             if (string.IsNullOrEmpty(propertyValue))
             {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Fields are empty",
-                    $"Error: '{field.Value}'",
-                    "OK"
-                );
-                return;
+                return false;
             }
         }
-
-        // Check if backflow passed or not
-        if (IsBackflowPassing())
-        {
-            // Save form data
-            Dictionary<string, string> formFields = new Dictionary<string, string>()
-            {
-                { "LinePressure", LinePressure ?? string.Empty },
-                { "SOVList", ShutoffValve ?? string.Empty },
-                { "SOVComment", SOVComment?.ToUpper() ?? string.Empty },
-                { "FinalCT1", decimal.TryParse(CheckValve1, out decimal cv1) ? cv1.ToString("F1") : string.Empty },
-                { "FinalCT2", decimal.TryParse(CheckValve2, out decimal cv2) ? cv2.ToString("F1") : string.Empty },
-                { "FinalRV", decimal.TryParse(PressureReliefOpening, out decimal rv) ? rv.ToString("F1") : string.Empty },
-                { "FinalCT1Box", (CheckValve1Leaked ? "Off" : "On") },
-                { "FinalCT2Box", (CheckValve2Leaked ? "Off" : "On") },
-            };
-            SaveFormData(formFields);
-
-            // Load next view model
-            var viewModel = new PassFinalViewModel();
-            viewModel.LoadPdfData(PdfData ?? throw new InvalidOperationException(),
-                FormData ?? throw new InvalidOperationException());
-            await Shell.Current.GoToAsync("PassFinal", new Dictionary<string, object>
-            {
-                { "ViewModel", viewModel }
-            });
-        }
-        else
-        {
-            // send to repair screen
-            await Application.Current.MainPage.DisplayAlert(
-                "Missing implementation",
-                $"The backflow failed, the next view model hasn't been implemented.",
-                "OK"
-            );
-            return;
-        }
+        
+        return true;
     }
-
-    #endregion
-
-    #region Private methods
-
-    private bool IsBackflowPassing()
+    
+    protected override bool IsBackflowPassing()
     {
         // Return false if any component has leaked or failed to open
         if (CheckValve1Leaked || CheckValve2Leaked || ReliefValveDidNotOpen)
@@ -208,8 +40,8 @@ public class RpTestViewModel : BaseBackflowViewModel
         }
 
         // Parse input values to decimal for numerical comparison
-        if (!decimal.TryParse(_checkValve1, out decimal checkValve1Value) ||
-            !decimal.TryParse(_pressureReliefOpening, out decimal reliefValveValue))
+        if (!decimal.TryParse(CheckValve1, out decimal checkValve1Value) ||
+            !decimal.TryParse(PressureReliefOpening, out decimal reliefValveValue))
         {
             return false; // Invalid input values
         }
@@ -235,6 +67,4 @@ public class RpTestViewModel : BaseBackflowViewModel
 
         return true;
     }
-
-    #endregion
 }
