0f09c46d0ec3e2a190141728042bdeed5dd2c399
muddxyii <muddxyii@gmail.com>
2024-11-30 10:48:20 -0700

    Fixed new sdk issues

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -3,6 +3,7 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace ABFReportEditor.WinUI;
 
 /// <summary>
