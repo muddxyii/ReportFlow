ef7e60af17cffb3c2e68663cdd1691887f291488
muddxyii <muddxyii@gmail.com>
2024-11-30 12:08:03 -0700

    Reorganized

--- ViewModels/TestViewModels/RpTestViewModel.cs
+++ ViewModels/TestViewModels/RpTestViewModel.cs
@@ -9,24 +9,8 @@ public class RpTestViewModel : BaseTestViewModel
         // Do base validation
         if (!base.ValidateFields()) return false;
         
-        // List of required fields with their error message
-        var requiredFields = new Dictionary<string, string>
-        {
-            { nameof(LinePressure), "Line Pressure" },
-            { nameof(ShutoffValve), "SOV Status" },
-            { nameof(CheckValve1), "CV1 Value" },
-            { nameof(PressureReliefOpening), "RV Opening Value" }
-        };
-
-        // Check string fields
-        foreach (var field in requiredFields)
-        {
-            var propertyValue = GetType().GetProperty(field.Key)?.GetValue(this) as string;
-            if (string.IsNullOrEmpty(propertyValue))
-            {
-                return false;
-            }
-        }
+        // Do RP Specific Checks
+        if (string.IsNullOrEmpty(CheckValve1) || string.IsNullOrEmpty(PressureReliefOpening)) return false;
         
         return true;
     }
@@ -34,11 +18,8 @@ public class RpTestViewModel : BaseTestViewModel
     protected override bool IsBackflowPassing()
     {
         // Return false if any component has leaked or failed to open
-        if (CheckValve1Leaked || CheckValve2Leaked || ReliefValveDidNotOpen)
-        {
-            return false;
-        }
-
+        if (CheckValve1Leaked || CheckValve2Leaked || ReliefValveDidNotOpen) return false;
+        
         // Parse input values to decimal for numerical comparison
         if (!decimal.TryParse(CheckValve1, out decimal checkValve1Value) ||
             !decimal.TryParse(PressureReliefOpening, out decimal reliefValveValue))
@@ -62,8 +43,6 @@ public class RpTestViewModel : BaseTestViewModel
         {
             return false;
         }
-
-        // TODO: Add shut off valve #2 checkstate
 
         return true;
     }
