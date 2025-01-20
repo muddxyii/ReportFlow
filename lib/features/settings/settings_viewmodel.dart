e3d7f6ce60f4879a4d169c251df364e651c623bd
muddxyii <muddxyii@gmail.com>
2025-01-20 12:39:44 -0700

    Implement proper app version extraction

--- lib/features/settings/settings_viewmodel.dart
+++ lib/features/settings/settings_viewmodel.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/foundation.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsViewModel extends ChangeNotifier {
   String _testerName = '';
   String _testKitSerial = '';
   String _testCertNo = '';
   String _repairCertNo = '';
+  String _appNameWithVersion = 'ReportFlow';
+
+  SettingsViewModel() {
+    _initializeAppNameWithVersion();
+  }
 
   // Getters
   String get testerName => _testerName;
   String get testKitSerial => _testKitSerial;
   String get testCertNo => _testCertNo;
   String get repairCertNo => _repairCertNo;
-  String get appNameWithVersion => 'ReportFlow v2.0.0';
+  String get appNameWithVersion => _appNameWithVersion;
   String get companyCopyright => '© 2024-${DateTime.now().year} AnyBackflow';
 
   // Setters with notification
@@ -32,6 +38,12 @@ class SettingsViewModel extends ChangeNotifier {
 
   set repairCertNo(String value) {
     _repairCertNo = value;
+    notifyListeners();
+  }
+
+  Future<void> _initializeAppNameWithVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    _appNameWithVersion = 'ReportFlow v${packageInfo.version}';
     notifyListeners();
   }
 
