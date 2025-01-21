9b6eafd6413326142c9d549857a2f316ec9b7f05
muddxyii <muddxyii@gmail.com>
2025-01-21 12:34:31 -0700

    Add basic .json file intention support with success opening settings page

--- android/app/src/main/kotlin/com/anybackflow/report_flow/MainActivity.kt
+++ android/app/src/main/kotlin/com/anybackflow/report_flow/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.anybackflow.report_flow
 
+import android.content.Intent
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "app_channel").setMethodCallHandler { call, result ->
+            if (call.method == "getInitialFilePath") {
+                val intent = activity.intent
+                result.success(intent.data?.path)
+            }
+        }
+    }
+}
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:report_flow/features/settings/presentation/settings_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    _handleIncomingJson();
+  }
+
+  Future<void> _handleIncomingJson() async {
+    const platform = MethodChannel('app_channel');
+    try {
+      final String? path = await platform.invokeMethod('getInitialFilePath');
+      if (path != null) {
+        if (mounted) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SettingsPage()),
+          );
+        }
+      }
+    } catch (e) {
+      debugPrint('Error handling JSON: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
