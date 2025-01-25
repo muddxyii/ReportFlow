12a3ddd0d38a1bcd388ae919e538d287b2491ac8
muddxyii <muddxyii@gmail.com>
2025-01-25 15:24:38 -0700

    Create default info_field wrapper

---
+++ lib/core/widgets/info_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class InfoField extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const InfoField({
+    required this.label,
+    required this.value,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label,
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        Text(value,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.normal,
+                )),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+}
