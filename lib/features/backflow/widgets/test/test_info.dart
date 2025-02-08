aaaad82fb8453ec10e85a1b4d2008e78a562a9fe
muddxyii <muddxyii@gmail.com>
2025-02-08 16:28:43 -0700

    feat(ui): add support for various device types in test info
    Why:
    - Previous implementation only partially handled device-specific UI.
    - Adds customized rendering logic for multiple device types (`dc`, `rp`, `pvb`, `svb`, `sc`, `type 2`).
    - Improves flexibility and UI clarity for displaying test information.

--- lib/features/backflow/widgets/test/test_info.dart
+++ lib/features/backflow/widgets/test/test_info.dart
@@ -27,16 +27,139 @@ class TestInfo extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Text('$label ',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
         Row(
           children: [
-            Text('$label ',
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    )),
-            _getCheckIcon(valve.closedTight)
+            if (valve.value.isNotEmpty)
+              Expanded(
+                child: Text('Val: ${valve.value}'),
+              ),
+            Expanded(
+              child: Row(
+                children: [
+                  Text('Ct: '),
+                  _getCheckIcon(valve.closedTight),
+                ],
+              ),
+            ),
           ],
         ),
-        Text('Val: ${valve.value}'),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+
+  Widget _buildReliefValveInfo(
+      BuildContext context, String label, ReliefValve rv) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$label ',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        Row(
+          children: [
+            if (rv.value.isNotEmpty)
+              Expanded(
+                child: Text('Val: ${rv.value}'),
+              ),
+            Expanded(
+              child: Row(
+                children: [
+                  Text('Opened: '),
+                  _getCheckIcon(rv.opened),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+
+  Widget _buildBackPressureInfo(
+      BuildContext context, String label, bool backPressure) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$label ',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        Row(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Text('Status: ${backPressure ? 'Yes' : 'No'}'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+
+  Widget _buildAirInletInfo(
+      BuildContext context, String label, AirInlet airInlet) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$label ',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        Row(
+          children: [
+            if (airInlet.value.isNotEmpty)
+              Expanded(
+                child: Text('Val: ${airInlet.value}'),
+              ),
+            Expanded(
+              child: Row(
+                children: [
+                  Text('Opened: '),
+                  _getCheckIcon(airInlet.opened),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+
+  Widget _buildCheckInfo(BuildContext context, String label, Check check) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$label ',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        Row(
+          children: [
+            Expanded(
+              child: Text('Val: ${check.value}'),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Text('Leaked: '),
+                  _getCheckIcon(check.leaked),
+                ],
+              ),
+            ),
+          ],
+        ),
         const SizedBox(height: 8),
       ],
     );
@@ -48,25 +171,57 @@ class TestInfo extends StatelessWidget {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child:
-                      _buildCheckValveInfo(context, 'CV-1', test.checkValve1),
-                ),
-                Expanded(
-                  child:
-                      _buildCheckValveInfo(context, 'CV-2', test.checkValve2),
-                )
-              ],
-            ),
+            _buildCheckValveInfo(context, 'CK #1', test.checkValve1),
+            _buildCheckValveInfo(context, 'CK #2', test.checkValve2),
           ],
         );
 
       case 'rp':
         return Column(
           children: [
-            Text('No rp support yet'),
+            _buildCheckValveInfo(context, 'CK #1', test.checkValve1),
+            _buildCheckValveInfo(context, 'CK #2', test.checkValve2),
+            _buildReliefValveInfo(context, 'RV', test.reliefValve),
+          ],
+        );
+
+      case 'pvb':
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildBackPressureInfo(
+                context, 'Back Pressure', test.vacuumBreaker.backPressure),
+            _buildAirInletInfo(
+                context, 'Air Inlet', test.vacuumBreaker.airInlet),
+            _buildCheckInfo(context, 'Check', test.vacuumBreaker.check),
+          ],
+        );
+
+      case 'svb':
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildBackPressureInfo(
+                context, 'Back Pressure', test.vacuumBreaker.backPressure),
+            _buildAirInletInfo(
+                context, 'Air Inlet', test.vacuumBreaker.airInlet),
+            _buildCheckInfo(context, 'Check', test.vacuumBreaker.check),
+          ],
+        );
+
+      case 'sc':
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildCheckValveInfo(context, 'CK #1', test.checkValve1),
+          ],
+        );
+
+      case 'type 2':
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildCheckValveInfo(context, 'CK #1', test.checkValve1),
           ],
         );
       default:
