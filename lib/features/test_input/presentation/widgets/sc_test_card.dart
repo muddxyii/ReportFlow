1242dac248f06e7e976b156fa446cd127504f7af
muddxyii <muddxyii@gmail.com>
2025-02-08 15:23:22 -0700

    feat(ui): add ScTestCard and Type2TestCard widgets

---
+++ lib/features/test_input/presentation/widgets/sc_test_card.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:report_flow/core/models/report_flow_types.dart';
+import 'package:report_flow/core/widgets/form_checkbox_field.dart';
+import 'package:report_flow/core/widgets/form_input_field.dart';
+
+class ScTestCard extends StatefulWidget {
+  final Test test;
+  final Function(Test) onTestUpdated;
+  final Function(String, FocusNode) addFocusNode;
+
+  const ScTestCard({
+    super.key,
+    required this.test,
+    required this.onTestUpdated,
+    required this.addFocusNode,
+  });
+
+  @override
+  State<ScTestCard> createState() => _ScTestCardState();
+}
+
+class _ScTestCardState extends State<ScTestCard> {
+  final Map<String, FocusNode> _focusNodes = {};
+  final List<String> _focusOrder = ['cv1Value'];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var key in _focusOrder) {
+      _focusNodes[key] = FocusNode();
+      widget.addFocusNode(key, _focusNodes[key]!);
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var node in _focusNodes.values) {
+      node.dispose();
+    }
+    super.dispose();
+  }
+
+  void _handleFieldSubmitted(String currentKey) {
+    final currentIndex = _focusOrder.indexOf(currentKey);
+    if (currentIndex < _focusOrder.length - 1) {
+      _focusNodes[_focusOrder[currentIndex + 1]]?.requestFocus();
+    }
+  }
+
+  Widget _buildCheckValveSection(
+    BuildContext context,
+    String title,
+    CheckValve checkValve,
+    String focusKey,
+    Function(CheckValve) onValveUpdated,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.titleMedium),
+        FormInputField(
+          label: 'Pressure Reading',
+          focusNode: _focusNodes[focusKey],
+          textInputType: TextInputType.number,
+          validateValue: true,
+          formatDecimal: true,
+          onChanged: (value) {
+            onValveUpdated(checkValve.copyWith(value: value ?? ''));
+          },
+          onSubmitted: () => _handleFieldSubmitted(focusKey),
+        ),
+        const SizedBox(height: 8),
+        FormCheckboxField(
+          label: 'Closed Tight',
+          value: checkValve.closedTight,
+          onChanged: (value) {
+            onValveUpdated(checkValve.copyWith(closedTight: value ?? false));
+          },
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildCheckValveSection(
+          context,
+          'Check Valve #1',
+          widget.test.checkValve1,
+          'cv1Value',
+          (valve) =>
+              widget.onTestUpdated(widget.test.copyWith(checkValve1: valve)),
+        ),
+      ],
+    );
+  }
+}
