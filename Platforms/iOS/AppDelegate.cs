92e037c1a3d6266cfd01c5a12b3372cb4645c164
muddxyii <muddxyii@gmail.com>
2025-02-11 20:10:45 -0700

    removed old 1.9.9 files

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ReportFlow;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
